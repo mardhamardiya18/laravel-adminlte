6eb5848b753f51a70a6ce55dbd40518e85737618
Mardiya Mardha <mardhamardiya.18@gmail.com>
2020-08-07 08:09:04 +0800

    tugas migration selesai

---
+++ database/migrations/2020_08_06_235047_create_komentar_jawaban_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKomentarJawabanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('komentar_jawaban', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('isi');
+            $table->date('tanggal_dibuat');
+            $table->date('tanggal_diperbarui');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('komentar_jawaban');
+    }
+}
